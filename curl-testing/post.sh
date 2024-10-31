f22ea0b26337cd92c0f234082046e90ea3ad31ed
REDplayer333HHH <horia.bosoanca@gmail.com>
2024-10-31 21:28:56 +0200

    security v2

--- curl-testing/post.sh
+++ curl-testing/post.sh
@@ -1,4 +1,4 @@
 curl -X POST http://localhost:3000/article \
 -H "Authorization: " \
 -H "Content-Type: application/json" \
--d '{"title": "New Article", "content": "This is the content of the article."}'
+-d '{"title": "Haha", "content": "Hihi"}'
--- server/security.go
+++ server/security.go
@@ -3,13 +3,33 @@ package server
 import (
 	"net/http"
 	"os"
+	"log"
 )
 
 func CheckApiKey(w http.ResponseWriter, r *http.Request) bool {
 	apiKey := r.Header.Get("Authorization")
 	if apiKey != os.Getenv("VICYBERAPIKEY") {
 		http.Error(w, "Unauthorized: Wrong API key!", http.StatusUnauthorized)
+		logApiKeyAttempt(apiKey, false)
 		return false
 	}
+	logApiKeyAttempt("Correct api key!", true)
 	return true
+}
+
+func logApiKeyAttempt(s string, succes bool) {
+	logFile, err := os.OpenFile("apikeylog.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		log.Printf("Error opening logfile: %v", err)
+		return
+	}
+	defer logFile.Close()
+
+	logger := log.New(logFile, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	if succes {
+
+	} else {
+
+	}
+	logger.Println(s)
 }
