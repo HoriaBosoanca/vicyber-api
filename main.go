f331ea4882ac10dc3b79d94b2b18ff75f1ab892f
REDplayer333HHH <horia.bosoanca@gmail.com>
2024-11-01 20:06:59 +0200

    enabled CORS

--- main.go
+++ main.go
@@ -18,10 +18,15 @@ func main() {
 	// database conectiviy
 	server.DB = server.ConnectDB()
 	server.DoMigrations(server.DB)
-	
+
+	// route
 	r := mux.NewRouter()
 	server.HandleArticle(r)
 
+	// CORS
+	r.Use(server.EnableCORS)
+
+	// log and serve
 	log.Println("Server starting on http://localhost:8010")
 	log.Fatal(http.ListenAndServe(":8010", r))
 }
--- server/security.go
+++ server/security.go
@@ -33,3 +33,18 @@ func logApiKeyAttempt(s string, succes bool) {
 	}
 	logger.Println(s)
 }
+
+func EnableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
