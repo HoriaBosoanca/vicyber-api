4026f7a6e283b7e75db2786dfa5e32b3d7c182b7
REDplayer333HHH <horia.bosoanca@gmail.com>
2024-11-03 11:40:01 +0200

    gitignore update

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-gcloud run deploy vicyberapi \
-  --source . \
-  --region europe-west3 \
-  --allow-unauthenticated \
-  --set-env-vars VICYBERAPIKEY=vicyberthebest,POSTGRES_URL=postgres://default:bPR6ofNAKJ5C@ep-super-flower-a2vcv1zv-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require
--- env.sh
+++
@@ -1,3 +0,0 @@
-gcloud run services update vicyberapi \
-  --region europe-west3 \
-  --set-env-vars VICYBERAPIKEY=vicyberthebest,POSTGRES_URL=postgres://default:bPR6ofNAKJ5C@ep-super-flower-a2vcv1zv-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require
