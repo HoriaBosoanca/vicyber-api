31d58344bfc3c74b2c5005964944722efe601f36
REDplayer333HHH <horia.bosoanca@gmail.com>
2024-11-03 11:47:12 +0200

    removed logfile

--- server/security.go
+++ server/security.go
@@ -3,30 +3,15 @@ package server
 import (
 	"net/http"
 	"os"
-	"log"
 )
 
 func CheckApiKey(w http.ResponseWriter, r *http.Request) bool {
 	apiKey := r.Header.Get("Authorization")
 	if apiKey != os.Getenv("VICYBERAPIKEY") {
 		http.Error(w, "Unauthorized: Wrong API key!", http.StatusUnauthorized)
-		logApiKeyAttempt(apiKey)
 		return false
 	}
-	logApiKeyAttempt("Correct api key!")
 	return true
-}
-
-func logApiKeyAttempt(s string) {
-	logFile, err := os.OpenFile("apikeylog.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil {
-		log.Printf("Error opening logfile: %v", err)
-		return
-	}
-	defer logFile.Close()
-
-	logger := log.New(logFile, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	logger.Println(s)
 }
 
 func OptionsHandler(w http.ResponseWriter, r *http.Request) {
