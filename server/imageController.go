6954892555f526174926b8d49c0887340aa7e77c
REDplayer333HHH <horia.bosoanca@gmail.com>
2024-11-07 22:51:25 +0200

    added ids to images

--- server/imageController.go
+++ server/imageController.go
@@ -4,7 +4,6 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -15,6 +14,7 @@ func HandleImage(router *mux.Router) {
 }
 
 type Image struct {
+	ID uint `gorm:"primaryKey"`
 	Data string `json:"data"`
 }
 
@@ -23,41 +23,38 @@ func createImage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// decode json into image var
-	var base64Img Image
-	err := json.NewDecoder(r.Body).Decode(&base64Img)
+	// decode json into image struct
+	var imageStruct Image
+	err := json.NewDecoder(r.Body).Decode(&imageStruct)
 	if err != nil {
 		http.Error(w, "Invalid input", http.StatusBadRequest)
 		return
 	}
 
-	// TODO:
-	imageData, err := decodeBase64Img(base64Img.Data)
+	imageDataBytes, err := decodeimageStruct(imageStruct.Data)
 	if err != nil {
 		http.Error(w, "Failed to decode image", http.StatusBadRequest)
 		return
 	}
-	log.Println(len(imageData))
-	imageurl, err := AddImageToBucket(imageData)
+	imageurl, err := AddImageToBucket(imageDataBytes)
 	if err != nil {
 		http.Error(w, "Failed to create image", http.StatusInternalServerError)
 		return
 	}
-	log.Println(imageurl)
 
-	imgurl := Image{Data: imageurl}
-	if DB.Create(&imgurl).Error != nil {
+	imageStruct.Data = imageurl
+	if DB.Create(&imageStruct).Error != nil {
 		http.Error(w, "Failed to create image", http.StatusInternalServerError)
 		return
 	}
 
 	// send base64 image back in json
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(imgurl)
+	json.NewEncoder(w).Encode(imageStruct)
 }
 
-func decodeBase64Img(base64ImgData string) ([]byte, error) {
-	imgData, err := base64.StdEncoding.DecodeString(base64ImgData)
+func decodeimageStruct(imageStructData string) ([]byte, error) {
+	imgData, err := base64.StdEncoding.DecodeString(imageStructData)
 	if err != nil {
 		return nil, fmt.Errorf("failed to decode image base64 data: %w", err)
 	}
